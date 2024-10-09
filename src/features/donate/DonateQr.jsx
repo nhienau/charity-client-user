ef90faa296f66b59c60a063491b06a9f4bee8588
Nhien <nhienau@gmail.com>
2024-10-09 15:25:39 +0700

    Error handling get zalopay payment URL

--- src/features/donate/DonateQr.jsx
+++ src/features/donate/DonateQr.jsx
@@ -15,19 +15,24 @@ function DonateQr() {
     );
   }
 
-  const { orderUrl } = data;
-
+  const { returnCode, orderUrl } = data;
   return (
     <>
       <div className="flex flex-col justify-between gap-4 md:col-span-3">
-        <div className="mx-auto my-0 max-w-64">
-          <a
-            className="flex items-center justify-center rounded-lg border-[1px] border-solid border-slate-400 px-6 py-3"
-            href={orderUrl}
-          >
-            Tiếp tục với ZaloPay
-          </a>
-        </div>
+        {returnCode === 1 ? (
+          <div className="mx-auto my-0 max-w-64">
+            <a
+              className="flex items-center justify-center rounded-lg border-[1px] border-solid border-slate-400 px-6 py-3"
+              href={orderUrl}
+            >
+              Tiếp tục với ZaloPay
+            </a>
+          </div>
+        ) : (
+          <div className="flex items-center justify-center">
+            <p>Có lỗi xảy ra, vui lòng thử lại.</p>
+          </div>
+        )}
         <div>
           <button
             className="flex grow-0 items-center gap-x-2 rounded-md border-[1px] border-solid border-slate-300 px-4 py-1.5"
